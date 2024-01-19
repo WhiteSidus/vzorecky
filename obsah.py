a0153e3844ad61297558797b502e471e676db90b
Whiteside <vovadxc@gmail.com>
2024-01-19 12:26:30 +0100

    prvni hodina

---
+++ obsah.py
@@ -0,0 +1,11 @@
+print ("Vítejte v aplikaci pro výpočet obsahu kruhu")
+#Deklarace proměnich a přetypovaní na int
+a = str(input("Zadejte poloměr: "))
+#Kontrolujeme zaporne čislo
+if a>0:
+    print("Výsledek je: ")
+    print(3.14*a*a)
+#Pokud nebude platit první podminka, splíse else
+else: 
+    print ("Chyba")
+
